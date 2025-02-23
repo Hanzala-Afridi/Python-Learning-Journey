285683bbed3f4edf3989700a01cae9ba2306f1d9
Hanzala <hanzalaafridi12345@gmail.com>
2025-02-23 12:33:34 -0800

    first commit

---
+++ 03-Day3-Our-First-Program.py
@@ -0,0 +1,24 @@
+#Day 4 - Our First Program
+
+print("Hello world")
+
+print(5)
+
+print("By")
+
+print(17*18)
+
+
+
+#Quick Quiz
+
+#Write a program to print a poem in Python. Choose the poem of your choice and publish 
+
+print("************ Nazm ************")
+print("")
+print("Chandni raaton mein hum ne dekha hai")
+print("Teri yaadon ka ujala sa hai")
+print("Hawa bhi teri baatein karti hai")
+print("Falak pe chand bhi tera naam likhta hai")
+print("")
+print("*******************************")
---
+++ 04-Day4-Comments-and-Print.py
@@ -0,0 +1,2 @@
+print ("I am a \"Good Boy\" \n and I am not bad boy")
+print("Afridi" , 8 , 5 , sep='~')
