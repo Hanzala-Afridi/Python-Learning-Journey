fd55df2aff8db26a4753082b12d3ca7d1e7d08e5
Hanzala <hanzalaafridi12345@gmail.com>
2025-03-01 01:35:18 -0800

    Added new file

---
+++ 18-Day18-Tuple in Python.py
@@ -0,0 +1,25 @@
+tup = (1, 2, 76, 342, 32, "green", True)
+# tup[0] = 90
+print(type(tup), tup)
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+print(tup[2])
+# print(tup[34])
+
+if  3421 in tup:
+  print("Yes 342 is present in this tuple")
+tup2 = tup[1:4]
+print(tup2)
+
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[3:7])     #using positive indexes
+print(animals[-7:-2])   #using negative indexes
+
+
+animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+print(animals[4:])      #using positive indexes
+print(animals[-4:])     #using negative indexes
+
+
